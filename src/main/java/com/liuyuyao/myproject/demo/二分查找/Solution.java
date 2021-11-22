439be46b2143ab9f4d43abd39ac87667d72ea0ee
刘禹尧 <513462482@qq.com>
2021-11-22 10:18:35 +0800

    修改二分查找

--- src/main/java/com/liuyuyao/myproject/demo/二分查找/Solution.java
+++ src/main/java/com/liuyuyao/myproject/demo/二分查找/Solution.java
@@ -1,5 +1,8 @@
 package com.liuyuyao.myproject.demo.二分查找;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 /**
  * @author : mi
  * create at:  2021/5/31  下午5:53
@@ -8,8 +11,9 @@ package com.liuyuyao.myproject.demo.二分查找;
 public class Solution {
     public int binarySearch(int[] nums, int target) {
         int low = 0, height = nums.length - 1;
+        int mid = 0;
         while (low <= height) {
-            int mid = (low + height) / 2;
+            mid = (low + height) / 2;
             if (target == nums[mid]) {
                 return mid;
             } else if (target > nums[mid]) {
@@ -18,6 +22,19 @@ public class Solution {
                 height = mid - 1;
             }
         }
-        return -1;
+        return mid;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] array = new int[]{1, 5, 8, 10};
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + "\t");
+        }
+        System.out.println("\n");
+        for (int target = 1; target <= 16; target++) {
+            int res = solution.binarySearch(array, target);
+            System.out.println("当target=" + target + "查找到的位置是" + res);
+        }
     }
 }
